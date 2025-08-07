7aa2aac1da6c8405586635b91f231c5312edf421
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-08-07 19:05:44 +0200

    Update repojump.rb

--- Formula/repojump.rb
+++ Formula/repojump.rb
@@ -1,11 +1,11 @@
 class Repojump < Formula
   desc "Quickly jump to your local GitHub repositories"
   homepage "https://github.com/acer51-doctom/repojump"
-  url "https://raw.githubusercontent.com/acer51-doctom/repojump/main/repojump"
+  url "https://raw.githubusercontent.com/acer51-doctom/repojump/main/RepoJump"
   version "1.0.0"
-  sha256 "f7503d48e015569155a5579d25af4dd57c3cb487e680ac9667c2178247002fac" # calculated below
+  sha256 "f7503d48e015569155a5579d25af4dd57c3cb487e680ac9667c2178247002fac"
 
   def install
-    bin.install "repojump"
+    bin.install "RepoJump" => "repojump"
   end
 end
